4d93951628dea839cb72bb602170ab544ec9a291
shashank <shekhershashank56@gmail.com>
2022-03-03 05:33:15 +0000

    dgdgdg

--- src/main.py
+++ src/main.py
@@ -36,4 +36,4 @@ def subscribe():
     return render_template('subscribe.html')
 
 if __name__ == "__main__":
-    app.run(host='13.126.64.105:5000',debug=True)
+    app.run(host='0.0.0.0',debug=True)
